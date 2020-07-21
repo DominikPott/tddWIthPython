0db9f61451dc86c6a0a72687ab137452fbc23fc2
DominikPott <d_tronic_p@gmx.de>
2020-07-21 21:07:20 +0200

    added database functional test

--- superlists/functional_test.py
+++ superlists/functional_test.py
@@ -15,6 +15,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has head aboub a cool new online to-do app. She goes
         # to che out its homepage
@@ -36,20 +41,18 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter, a page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-
         WebDriverWait(self.browser, 10).until(
             expected_conditions.text_to_be_present_in_element(
                 (By.ID, 'id_list_table'), 'Buy peacock feathers')
         )
 
-        table = self.browser.find_element_by_id("id_list_table")
-        rows = table.find_elements_by_tag_name("tr")
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a textbox inviting her do add another item. The
         # enters "Use peacock feathers to make a fly" Edith is very methodical
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
+
         inputbox.send_keys(Keys.ENTER)
         WebDriverWait(self.browser, 10).until(
             expected_conditions.text_to_be_present_in_element(
@@ -57,10 +60,8 @@ class NewVisitorTest(unittest.TestCase):
         )
 
         # The page updates again and now shows both items on her list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_element_by_tag_name('tr')
-        #self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
 
         # Edith wonders whether the site will remember her list. Then she sees
